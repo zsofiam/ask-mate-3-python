fcf85978801583d4e2dba6ae3d140258c9f99adc
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-01-18 16:24:33 +0100

    merge listing changes with question routing base

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,9 @@
 import csv
 
 QUESTIONS_FILE_PATH = 'sample_data/question.csv'
+ANSWERS_FILE_PATH = "sample_data/answer.csv"
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
 
 def get_all_questions_from_file():
     all_questions = []
@@ -9,11 +11,28 @@ def get_all_questions_from_file():
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             all_questions.append(row)
-    return  all_questions
+    return all_questions
 
 
 def get_questions_sorted_by_submission_date():
     questions = get_all_questions_from_file()
     sorted_questions = sorted(questions, key=lambda  k: k['submission_time'], reverse=True)
     return sorted_questions
+
+
+def get_one_question(q_id):
+    all_questions = get_all_questions_from_file()
+    for question in all_questions:
+        if question['id'] == q_id:
+            return question
+
+
+def get_answers(q_id):
+    answers = []
+    csv_file = open(ANSWERS_FILE_PATH)
+    csv_answers = csv.DictReader(csv_file)
+    for answer in csv_answers:
+        if answer['question_id'] == q_id:
+            answers.append(answer)
+    return answers
 
--- server.py
+++ server.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
 import data_manager
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
+def main_page():
+    return redirect("/list")
+
+
 @app.route('/list')
 def route_list():
     questions_list = data_manager.get_questions_sorted_by_submission_date()
     return render_template('list.html', questions=questions_list)
 
 
+@app.route("/question/<int:question_id>")
+def display_question(question_id):
+    question = data_manager.get_one_question(question_id)
+    answers = data_manager.get_answers(question_id)
+    return render_template('q_and_a.html', question=question, answers=answers)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug=True
+    )

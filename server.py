cdf8971264597ae5e8e68ee03640c175da9a7f90
csilla-hegedus <hegeduscsilla333@gmail.com>
2021-02-03 11:52:27 +0100

    rearrange search route

--- server.py
+++ server.py
@@ -10,15 +10,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
-def main_page():
-    questions = data_manager.get_latest_five_questions()
-    return render_template('index.html', questions=questions)
-
-
 @app.route('/search')
+def main_page():
+    results = []
+    query_string = request.args
+    if 'q' in query_string and query_string['q'] != '':
+        word = query_string['q']
+        results = data_manager.search(word)
+    questions = data_manager.get_latest_five_questions()
+    return render_template('index.html', questions=questions, results=results)
+
+
 @app.route('/list')
 def route_list():
-    results = []
     order_by = 'submission_time'
     order_direction = 'desc'
     args = request.args
@@ -26,11 +30,9 @@ def route_list():
         order_by= args["order_by"]
     if 'order_direction' in args:
         order_direction = args['order_direction']
-    if 'q' in args and args['q'] != '':
-        word = args['q']
-        results = data_manager.search(word)
+
     questions_list = data_manager.get_questions_sorted(order_by,order_direction)
-    return render_template('list.html', questions=questions_list, results=results)
+    return render_template('list.html', questions=questions_list)
 
 
 @app.route("/question/<question_id>")
